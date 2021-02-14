0f8d74f3d8fd3967a83987cfcddd365c7a1f03c5
bak8072 <78713539+bak8072@users.noreply.github.com>
2021-02-14 00:32:33 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -5,8 +5,6 @@
 def welcome_assignment_answers(question):
     # The student doesn't have to follow the skeleton for this assignment.
     #    #Another way to implement is using a "case" statements similar to C.
-
-    global answer
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
@@ -15,17 +13,17 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
-    elif question == "What is the MD5 hashing value to the following message:'NYU Computer Networking' - Use " + \
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use " + \
             "MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? -" + \
-            "The answer should be a numeric number":
-        answer = "4"
+            " The answer should be a numeric number":
+        answer = "Application layer"
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? -" + \
             "The answer should be a numeric number":
-        answer = "3"
+        answer = "Transport Layer"
     return answer
 
 
@@ -36,3 +34,4 @@ if __name__ == "__main__":
     # use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
+
