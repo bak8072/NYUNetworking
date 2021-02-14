7c19dc5ed894a4d566a2cfb78cb2d0d04cc55961
bak8072 <78713539+bak8072@users.noreply.github.com>
2021-02-13 23:58:16 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -17,9 +17,9 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use " + \
             "MD5 hash generator and use the answer in your code":
-        answer = "b21bed4510d4c731cf6a8afe2c2bc071"
+        answer = 'b21bed4510d4c731cf6a8afe2c2bc071'
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? -" + \
             " The answer should be a numeric number":
         answer = "4"
