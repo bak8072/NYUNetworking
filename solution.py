6aca57c455e0a5213cbdcd35c069823a9e1fd0c7
bak8072 <78713539+bak8072@users.noreply.github.com>
2021-02-14 12:33:35 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -18,13 +18,13 @@ def welcome_assignment_answers(question):
             "MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? -" + \
-            " The answer should be a numeric number":
-        answer = "4"
+            "The answer should be a numeric number":
+        answer = "5"
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? -" + \
             "The answer should be a numeric number":
-        answer = "3"
+        answer = "4"
     return answer
 
 
